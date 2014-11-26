6b40a4b38b833a297f3717987c7fd0a23cd499b3
jkhun47 <jkhun47@hotmail.com>
2014-11-26 10:00:58 +0900

    welcome.php

---
+++ welcome.php
@@ -0,0 +1,306 @@
+
+<html>
+<head>
+<title>G&G Service</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="Description" content="スマートフォンアプリ開発、ウェブコンサルティン グ、ITテクニカルサポートサービスを提供します。">
+<meta name="Keywords" content="スマートフォンアプリ開発,ウェブコンサルティン グ,絵本,iPad,iPhone,電子絵本,SI事業,iPadアプリ,iMo">
+<link href="../common_img/favicon.ico" rel="icon" type="image/x-icon"/>
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+<!-- <script language="javascript" src="../inc/object.js"></script> -->
+<script type="text/JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+	var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+	var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+	var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+		if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+	var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+	d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+	if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+	for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+	if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+	if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+	document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+	else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+
+function MM_swapImage() { //v3.0
+var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+	if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+//スマートフォンのpopup
+function SmartPhone() {
+var nLink = '../07_member/popup_phone.jsp';
+	var nTarget = 'popup';
+	var nWidth = 618;
+var nHeight = 600;
+var xPos = 0;
+var yPos = 0;
+if(typeof nLink == "object") url = nLink.href;
+else url = nLink;
+
+adjX = xPos ? xPos : (window.screen.width/2 - nWidth/2);
+adjY = yPos ? yPos : (window.screen.height/2 - nHeight/2 - 100);
+var qResult = window.open( url, nTarget, "width="+nWidth+", height="+nHeight+",left="+adjX+",top="+adjY+",toolbar=no,status=no,scrollbars=yes,resizable=no, location=no");
+}
+
+//-->
+</script>
+</head>
+
+<body background="../common_img/back_body.gif" onLoad="MM_preloadImages('common_img/top_menu01_ov.gif','common_img/top_menu02_ov.gif','common_img/top_menu03_ov.gif','common_img/top_menu04_ov.gif')">
+<table width="880" height="100%" border="0" cellspacing="0" cellpadding="0" align="center"  bgcolor="#FFFFFF">
+  <tr>
+  <td background="../common_img/body_left.gif" width="15"></td>
+	<td valign="top" width="850"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+	<td height="70"><?php include "../inc/top.php"?><%@include file="../inc/top.php"%></td>
+	</tr>
+      <tr>
+	<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+	<td><table width="239" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+	<td><a href="../01_company/01company_01.jsp" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image1','','../common_img/top_menu01_ov.gif',1)"><img src="../common_img/top_menu01.gif" alt="会社紹介" name="Image1" width="239" height="53" border="0"></a></td>
+              </tr>
+              		<tr>
+                <td><a href="../02_service/02service_01.jsp" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image2','','../common_img/top_menu02_ov.gif',1)"><img src="../common_img/top_menu02.gif" alt="サービス案内" name="Image2" width="239" height="53" border="0"></a></td>
+                		</tr>
+                				<tr>
+                						<td><a href="../03_support/03support_01_list.jsp" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image3','','../common_img/top_menu03_ov.gif',1)"><img src="../common_img/top_menu03.gif" alt="サポートセンタ（Q&A）" name="Image3" width="239" height="53" border="0"></a></td>
+                								</tr>
+                								<tr>
+                								<td><a href="../04_recruit/04recruit_01.jsp" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image4','','../common_img/top_menu04_ov.gif',1)"><img src="../common_img/top_menu04.gif" alt="採用情報" name="Image4" width="239" height="53" border="0"></a></td>
+                										</tr>
+                										</table></td>
+                										<td width="611">
+                										<embed src="../common_img/main_flash.swf" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="611" height="212">
+                										<!--
+                										<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="611" height="212">
+                										<param name="movie" value="../common_img/main_flash.swf" />
+                										<param name="quality" value="high" />
+                										<embed src="../img_main/ichibaProduct.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="611" height="212"></embed>
+                										</object> -->
+                										</td>
+                										</tr>
+                										</table></td>
+      </tr>
+      <tr>
+      <td style="padding: 30px 30px 30px 30px;" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+      <td valign="top"><table width="232" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+      		<td><a href="javascript:SmartPhone();"><img src="../common_img/banner_01.gif" alt="スマートフォン利用状況" width="232" height="109" border="0"></a></td>
+				<!--<td><img src="common_img/banner_01.gif" alt="サービス紹介" width="232" height="109" border="0" onClick="window.open('../07_member/popup_personal.jsp', 'new', 'width=480px, height=600px, left=100px, top=100px, scrollbars=yes');" style="cursor:pointer"></td>-->
+				</tr>
+				<tr>
+				<td height="10"></td>
+				</tr>
+				<tr>
+				<td><a href="mailto:master@gngs.co.jp"><img src="../common_img/banner_02.gif" alt="お問い合わせ" width="232" height="46" border="0"></a></td>
+				</tr>
+				<tr>
+				<td height="5"></td>
+				</tr>
+				<tr>
+				<td><a href="../catalog/IBS_catalog.pdf"><img src="../common_img/banner_03.gif" alt="ダウンロード" width="232" height="46" border="0"></a></td>
+				</tr>
+				<tr>
+				<td height="10"></td>
+				</tr>
+				<tr>
+				<td>
+				<a href="http://www.imo-service.com/jp/introduce.imo" target="_blank" onfocus="this.blur()">
+				<img src="../common_img/imo_info.gif" alt="iMoサービス開始" width="178" height="119" border="0">
+				</a>
+				</td>
+				</tr>
+				</table></td>
+				<td width="28"></td>
+				<td width="530" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+				<tr>
+				<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+				<tr>
+				<td height="30" background="../common_img/title_back.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+				<tr>
+				<td><img src="../common_img/title_notice.gif" width="78" height="24"></td>
+				<td align="right"><a href="../05_notice/05notice_01_list.jsp"><img src="../common_img/btn_more.gif" alt="詳しく見る" width="57" height="13" border="0"></a></td>
+				</tr>
+				</table></td>
+				</tr>
+				<tr>
+				<td height="10"></td>
+				</tr>
+				<!-- お知らせ表示処理Start -->
+				<%
+				for (int i=0; i<list.size(); i++) {
+				NoticeDTO noticedto = (NoticeDTO) list.get(i);
+				String write_date = noticedto.getWrite_date();
+				String year = write_date.substring(0,4);
+				String month = write_date.substring(5,7);
+				String day = write_date.substring(8,10);
+				%>
+				<tr>
+				<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+				<tr>
+				<td width="16" height="30"><img src="../common_img/point_dot.gif"></td>
+						<td width="125"><b>[<%=year%>年<%=month%>月<%=day%>日]</b></td>
+						<td><a href="../05_notice/05notice_01_view.jsp?num=<%=noticedto.getNum()%>&pageNum=1"><%=noticedto.getTitle()%></a></td>
+                      </tr>
+								</table></td>
+								</tr>
+								<tr>
+								<td height="1px" background="../common_img/line_dot.gif"></td>
+								</tr>
+								<%
+				}
+				%>
+				<!-- お知らせ表示処理End -->
+
+				<!--notice start
+						<tr>
+						<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+						<tr>
+						<td width="16" height="30"><img src="../common_img/point_dot.gif"></td>
+						<td width="125"><b><a href="../05_notice/notice_06.jsp">[2010年11月18日]</a></b></td>
+						<td><a href="../05_notice/notice_06.jsp">iPhoneとiPadアプリランキングサイト「BuzzApp」サイトにて好評</a></td>
+						</tr>
+                    </table></td>
+                    </tr>
+                    <tr>
+                    <td height="1px" background="../common_img/line_dot.gif"></td>
+                    </tr>
+                    		<tr>
+                    		<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                      <tr>
+                      <td width="16" height="30"><img src="../common_img/point_dot.gif"></td>
+                        <td width="125"><b><a href="../05_notice/notice_05.jsp">[2010年09月15日]</a></b></td>
+                        <td> <a href="../05_notice/notice_05.jsp">「ももたろう for iPad」アプリNewリリースと注目作品に登場</a></td>
+                        		</tr>
+                        				</table></td>
+                        				</tr>
+                        				<tr>
+                        						<td height="1px" background="../common_img/line_dot.gif"></td>
+                        								</tr>
+                        								<tr>
+                        								<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        								<tr>
+                        										<td width="16" height="30"><img src="../common_img/point_dot.gif"></td>
+                        										<td width="125"><b><a href="../05_notice/notice_04.jsp">[2010年09月19日]</a></b></td>
+                        										<td><a href="../05_notice/notice_04.jsp">絵本の新しいカタチ 「MOMOTARO for iPad」</a></td>
+                        										</tr>
+                        										</table></td>
+                        										</tr>
+                        										<tr>
+                        										<td height="1px" background="../common_img/line_dot.gif"></td>
+                        										</tr>
+                        										<tr>
+                        												<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        												<tr>
+                        												<td width="16" height="30"><img src="../common_img/point_dot.gif"></td>
+                        														<td width="125"><b><a href="../05_notice/notice_03.jsp">[2010年09月18日]</a></b></td>
+                        														<td><a href="../05_notice/notice_03.jsp">AppStoreへ「ももたろう for iPad」リリース！！</a></td>
+                        														</tr>
+                        														</table></td>
+                        														</tr>
+                        														<tr>
+                        														<td height="1px" background="../common_img/line_dot.gif"></td>
+                        														</tr>
+                        														<!--notice end-->
+
+                        														</table></td>
+                        														</tr>
+                        														<tr>
+                        														<td>&nbsp;</td>
+                        														</tr>
+                        														<tr>
+                        														<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        														<tr>
+                        														<td height="30" background="../common_img/title_back.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        														<tr>
+                        														<td><img src="../common_img/title_iphone.gif"></td>
+                        														<td align="right"><a href="../06_app/06app_01.jsp"><img src="../common_img/btn_more.gif" alt="詳しく見る" width="57" height="13" border="0"></a></td>
+                        														</tr>
+                        														</table></td>
+                        														</tr>
+                        														<tr>
+                        														<td height="10"></td>
+                        														</tr>
+                        														<!--notice start-->
+                        																<tr>
+                        																		<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        																		<tr>
+                        																		<td width="32%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        																		<tr>
+                        																		<td rowspan="2" width="78"><a href="../06_app/06app_01.jsp#007"><img src="../06_app/img/app_icon07.png" alt="iMo Navi" border="0"></a></td>
+                        																		<td width="5">&nbsp;</td>
+                        																		<td height="25"><b><a href="../06_app/06app_01.jsp#007" style="font-size:14px;">iMo Navi<br>&nbsp;</a></b></td>
+                        																		</tr>
+                        																		<tr>
+                        																		<td>&nbsp;</td>
+							<td valign="top"><a href="../06_app/06app_01.jsp#007">2011.7.27<br>発売</a><br>
+							</td>
+							</tr>
+							</table></td>
+							<td width="10">&nbsp;</td>
+									<td width="32%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+									<tr>
+									<td rowspan="2" width="78"><a href="../06_app/06app_01.jsp#009"><img src="../06_app/img/app_icon09.png" alt="トトくんのほっぺ" border="0"></a></td>
+									<td width="5">&nbsp;</td>
+									<td height="25"><b><a href="../06_app/06app_01.jsp#009" style="font-size:14px;">トトくんのほっぺ</a></b></td>
+									</tr>
+									<tr>
+									<td>&nbsp;</td>
+									<td valign="top"><a href="../06_app/06app_01.jsp#009">2011.8.15<br>発売<br></a></td>
+									</tr>
+											</table></td>
+											<td width="10">&nbsp;</td>
+											<td width="35%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+											<tr>
+							<td rowspan="2" width="78"><a href="../06_app/06app_01.jsp#008"><img src="../06_app/img/app_icon08.png" alt="エリックへのプレゼント" border="0"></a></td>
+							<td width="5">&nbsp;</td>
+							<td height="25"><b><a href="../06_app/06app_01.jsp#008" style="font-size:12px;">エリックへの<br>プレゼント</a></b></td>
+							</tr>
+							<tr>
+							<td>&nbsp;</td>
+							<td valign="top"><a href="../06_app/06app_01.jsp#008">2011.8.15<br/>発売</a></td>
+							</tr>
+							</table></td>
+							</tr>
+							</table></td>
+							</tr>
+							<!--notice end-->
+							</table></td>
+							</tr>
+							</table></td>
+							</tr>
+							</table></td>
+							</tr>
+							 
+							<!--footer start-->
+									<tr>
+									<td height="90"><?php include "../inc/footer.jsp"?><%@include file="../inc/footer.jsp" %></td>
+									</tr>
+									<!-- footer end-->
+	 
+    </table></td>
+	<td background="../common_img/body_right.gif" width="15"></td>
+  </tr>
+</table>
+</body>
+</html>
+
+
+<?php?>
